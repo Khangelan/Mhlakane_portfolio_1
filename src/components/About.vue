3b892c93b1ad6ac2bf9dbc770a5961de89f72751
Khangelani Mhlakane <kmhlakane@gmail.com>
2026-07-14 16:09:29 +0200

    cv updated

--- src/components/About.vue
+++ src/components/About.vue
@@ -29,7 +29,7 @@
           <span>Oracle Apex</span>
         </div>
 
-        <a href="/public/downloads/Khangelani Mhlakane CV (1).pdf" download class="download-btn">
+        <a href="/public/downloads/Khangelani_Mhlakane.pdf" download class="download-btn">
           <i class="fa-solid fa-arrow-down-to-bracket"></i> Download CV
         </a>
       </div>
